720d5a0c1b0e46fa10a9ff6d139319823fbcfd66
xxoh <xxoh0302@naver.com>
2024-09-20 16:49:01 +0900

    Update test3.py

--- views/test3.py
+++ views/test3.py
@@ -11,12 +11,11 @@ def playGame(guessNum):
   
 st.title("동전 던지기 게임")
 st.divider()
+st.image('assets/coin_head.png')
+st.image('assets/coin_tail.png')
 
-st. image('assets/coin_head.png')
-st. image('assets/coin_tail.png')
-
-st. header('동전 던지기 게임에 오신 것을 환영합니다.')
-st. subheader('앞면일까요? 뒷면일까요?')
+st.header('동전 던지기 게임에 오신 것을 환영합니다.')
+st.subheader('앞면일까요? 뒷면일까요?')
 
 if st.button('앞면'):
   playGame(0)
